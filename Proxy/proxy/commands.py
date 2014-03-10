0d1362cf9fa8bf88a3ab4df4f0471362cdeac6f0
James Gebbie <james.gebbie@stfc.ac.uk>
2014-03-10 16:57:21 +0000

    Create connections and commands classes

---
+++ Proxy/proxy/commands.py
@@ -0,0 +1,16 @@
+
+
+class Commands:
+    """A class of commands that can be invoked on the remote machine"""
+    
+    # A function for submitting jobs
+    def submit(self):
+        print("submit")
+        
+    # A function for deleting jobs    
+    def delete(self):
+        print("delete")
+        
+    # A function for querying jobs
+    def status(self):
+        print("status")
---
+++ Proxy/proxy/connections.py
@@ -0,0 +1,14 @@
+import os
+
+class Connections:
+    """A class containing all the connection related functions"""
+    
+    # Open function is defined here
+    def openconnection(self):
+        print("open something")
+     
+    # Close function is defined here   
+    def closeconnection(self):
+        print("close it")
+    
+    
