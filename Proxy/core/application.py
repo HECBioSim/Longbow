282fc793e4d7ec0fce7975bb656b384ca018f8a2
James Gebbie <james.gebbie@stfc.ac.uk>
2014-03-25 16:56:46 +0000

    EOD Commit

--- Proxy/core/application.py
+++ Proxy/core/application.py
@@ -1,7 +1,16 @@
-
+import argparse
 
 class Amber:
-    pass
+    
+    parser = argparse.ArgumentParser()
+    
+    parser.add_argument("-i", "--test1", help = "Name of the computer resource for example Archer.", required = True)
+    parser.add_argument("-j", "--test2", help = "Name of the software for example Amber.", required = True)
+    parser.add_argument("-k", "--test3", help = "Name of the computer resource for example Archer.", required = True)
+    
+    #remnants = parser.parse_args(remnants)
+    
+    #print(remnants)
     
 class Charmm:
     pass
--- Proxy/proxy.py
+++ Proxy/proxy.py
@@ -1,8 +1,5 @@
-import os
-from core.systemcommands import SysCommands
-from core.config import RemoteConfig
-    
-def proxy(args, config_file):
+
+def proxy(args, remnant_args, config_file):
 
     #Use paths relative to user dir so set this as our cwd
     os.chdir(os.path.expanduser("~"))
@@ -10,7 +7,7 @@ def proxy(args, config_file):
     remote = RemoteConfig(args, config_file)
     command = SysCommands(remote.user, remote.host, remote.port)
 
-    test = ["ls"]
+    #test = ["ls"]
 
     #command.sshconnection(test)
     #command.runcommand(test)
@@ -18,4 +15,25 @@ def proxy(args, config_file):
     
     print("resource = ", args.resource)
     print("program = ", args.program)
+
+if __name__ == "__main__":
+    """Main entry point for the ProxyApp as a stand-alone application. The main function proxy can be hooked directly by providing it with the correct args."""
+    import os
+    import argparse
+    from core.systemcommands import SysCommands
+    from core.config import RemoteConfig
+    
+    #Instantiate the parser
+    parser = argparse.ArgumentParser(description = "Welcome to the ProxyApp.")
+    
+    #Parse the command line args
+    parser.add_argument("-res", "--resource", help = "Name of the computer resource for example Archer.", required = True)
+    parser.add_argument("-prog", "--program", help = "Name of the software for example Amber.", required = True)
+    #parser.add_argument(nargs = "*")
+    
+    args, remnants = parser.parse_known_args()
+
+    config_file = "./Workspace/Git/ProxyApp/Proxy/settings.conf"
+    
+    proxy(args, remnants, config_file)
 
