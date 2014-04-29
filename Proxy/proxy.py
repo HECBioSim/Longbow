72c14080fa51952da453e2623d998c903392625f
James Gebbie <james.gebbie@stfc.ac.uk>
2014-04-29 12:51:19 +0100

    update

--- Proxy/proxy.py
+++ Proxy/proxy.py
@@ -6,7 +6,7 @@ from core.appcommands import Applications
 from core.jobcommands import Scheduler
 from core.staging import Staging
 
-def proxy(app_args, debug):
+def proxy(app_args, debug, configfile, jobfile, logfile):
     
     """The main function that forms the basis for operating the core library. This can serve as a template
     for building more advanced applications. Here most of the classes are doing auto configuration through 
@@ -18,30 +18,37 @@ def proxy(app_args, debug):
     #TODO: Replace sys.exits throughout the application with exception raising. Also this would be a good time to introduce the logfile.
     #TODO: Fix issues with files that are not being uploaded sometimes, this must be something failing somewhere (possibly ssh or scp) all commands should have their returns checked.
     
+    
     #-----------------------------------------------------------------------------------------------
     #I find it easier using relative paths, in this case I'm going to run both the remote and local
     #paths relative to the "~" user dir. Before I set the working dir to the user dir, I capture the
     #absolute path of the hosts.conf and job.conf.
     
+    
     #Current dir to get the 
     currentdir = os.getcwd()
     
-    #host config file (user, host, port and scheduler)
-    configfile = currentdir + "/hosts.conf"
+    #Host config file (user, host, port and scheduler)
+    if(configfile == ""): configfile = currentdir + "/hosts.conf"
     
     #TODO: add this as a commandline arg -conf (like Charlie's app) which means that I want to use a relative path too.
-    jobfile = currentdir + "/job.conf"
+    if(jobfile == ""): jobfile = currentdir + "/job.conf"
     
     #Logfile for troubleshooting.
     #TODO: this log file isn't yet used, include it when sys.exits are replaced by exception handling.
-    logfile = open(currentdir + "/log", "w+")
+    if(logfile == ""): logfile = currentdir + "/log"
+        
+    logfile = open(logfile, "w+")
 
     #Use paths relative to user dir so set this as our cwd
     os.chdir(os.path.expanduser("~"))
     
+    
     #-----------------------------------------------------------------------------------------------
     #Instantiate the classes.
     
+    
+    #Load the job configuration file.
     jobconf = JobConfig(jobfile)
     
     #Instantiate the remote connection configuration class. This is where host connections are dealt with
@@ -62,8 +69,10 @@ def proxy(app_args, debug):
     #Some tests as to whether the application is actually in your path will happen automatically.
     application = Applications.test(command, jobconf.program, jobconf.executable)
     
+    
     #-----------------------------------------------------------------------------------------------
     #Start processing the job setup and submit.
+    
     
     #Process the command line args to separate out all the files that need staging and form a nice string for the scheduler.
     filelist, arglist = application.processjob(app_args)
@@ -78,20 +87,25 @@ def proxy(app_args, debug):
     #Submit the job to the scheduler.
     jobid = job.submit(command, jobconf.remote_workdir, submitfile)
     
+    
     #-----------------------------------------------------------------------------------------------
     #Monitor jobs.
     
+    
     job.monitor(command, stage, jobconf.frequency, jobid, jobconf.local_workdir, jobconf.remote_workdir)
+    
     
     #-----------------------------------------------------------------------------------------------
     #Final transfer of data and clean up.
+    
 
     #Download final results.
     stage.stage_downstream(command, jobconf.local_workdir, jobconf.remote_workdir)
     
-    #TODO: add in the remove files part.
-    
-    
+    #Remove the remote directory.
+    command.removefileremote(jobconf.remote_workdir)
+
+
 if __name__ == "__main__":
     
     """Main entry point for the ProxyApp as a stand-alone application. The main function proxy can be hooked directly by providing it with the correct args."""
