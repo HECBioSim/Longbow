8336867c9d323d0f508db5acf79fcf3455881e78
gareths <gareths@taggart.pharm.nottingham.ac.uk>
2015-03-11 15:15:32 +0000

    Updated setup.py

--- setup.py
+++ setup.py
@@ -6,18 +6,25 @@ import os
 
 from distutils.core import setup
 import sys
+import subprocess
 
+# check python version
 print "Checking python version!"
-
 if not (sys.version_info[0] >= 2 and sys.version_info[1] >= 7):
     exit_with_error("You should install Python 2.7 or greater to install "
                     "Longbow")
-
 print "Python version checked."
 
+# download examples
+subprocess.call(["wget", "-P", os.path.expanduser("~"), 
+                 "http://www.hecbiosim.ac.uk/longbow-extras.zip"])
+subprocess.call(["unzip", "-d", os.path.expanduser("~"), 
+                 os.path.expanduser("~/longbow-extras.zip")])
+
+# setup args
 setup_args = {
     'name'             : "Longbow",
-    'version'          : "0.0.9",
+    'version'          : "0.9.9",
     'description'      : "Biomolecular simulation remote job submission "
                             "utility.",
     'long_description' : "Longbow sends jobs submitted to your desktop to a "
@@ -25,8 +32,6 @@ setup_args = {
                             " brought back bringing the power of an HPC to "
                             "your desktop",
     'author'           : "James T Gebbie-Rayet and Gareth B Shannon",
-    'url'              : "https://bitbucket.org/jimboid/longbow",
-    'download_url'     : "https://bitbucket.org/jimboid/longbow/downloads",
     'license'          : "GNU General Public License.",
     'classifiers'      : [
         'Development Status :: 4 - Beta',
@@ -45,7 +50,6 @@ setup_args = {
                      "Longbow.plugins.schedulers", "Longbow.corelibs"],
     'scripts' : ['Longbow/longbow'],
 
-    'zip_safe'         : False,
 }
 
 setup(**setup_args)
