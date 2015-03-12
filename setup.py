88c4419da8cbaacc0a84417e72b27decc2c8a9d4
gareths <gareths@taggart.pharm.nottingham.ac.uk>
2015-03-12 16:14:52 +0000

    Small Fix

--- setup.py
+++ setup.py
@@ -35,20 +35,21 @@ print "Checking python version!"
 if not (sys.version_info[0] >= 2 and sys.version_info[1] >= 7):
     exit_with_error("You should install Python 2.7 or greater to install "
                     "Longbow")
-
+    
 # download examples
 print "Downloading example files"
 
-subprocess.call(["wget", "-P", os.path.expanduser("~"),
-                 "http://www.hecbiosim.ac.uk/longbow-extras.zip"])
+if not os.path.isdir(os.path.expanduser("~/Longbow")):
+    subprocess.call(["wget", "-P", os.path.expanduser("~"),
+                     "http://www.hecbiosim.ac.uk/longbow-extras.zip"])
 
-subprocess.call(["unzip", "-d", os.path.expanduser("~"),
-                 os.path.expanduser("~/longbow-extras.zip")])
+    subprocess.call(["unzip", "-d", os.path.expanduser("~"),
+                     os.path.expanduser("~/longbow-extras.zip")])
 
 # setup args
 setup_args = {
     'name': "Longbow",
-    'version': "0.9.9",
+    'version': "0.9.98",
     'description': "Biomolecular simulation remote job submission "
                    "utility.",
     'long_description': "Longbow sends jobs submitted to your desktop to a "
