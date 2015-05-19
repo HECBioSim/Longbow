b966dfcb2aef5dd60de6d0b77edecb2db11576ee
gareths <gareths@taggart.pharm.nottingham.ac.uk>
2015-05-19 12:36:40 +0100

    Better commenting for parsers

--- Longbow/plugins/apps/lammps.py
+++ Longbow/plugins/apps/lammps.py
@@ -40,8 +40,7 @@ def file_parser(filename, path, files, substitutions=None):
     Recursive function that will assimilate from lammps input files a list of
     files (files) to be staged to the execution host. filename will be added
     to the list and any files mentioned in filename will also be added and
-    searched. Substitutions is a dictionary of "$" style variables specified
-    on the command line.
+    searched. Substitutions is a dictionary of "$" style variables.
     '''
 
     # Check the location of filename
@@ -173,6 +172,10 @@ def file_parser(filename, path, files, substitutions=None):
 
 
 def sub_dict(args):
+    '''
+    Function to detect substitutions specified on the commandline.
+    '''
+    
     sub = {}
     for index, item in enumerate(args):
         if item == "-var" or item == "-v":
