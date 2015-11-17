0d70a0aac744b04437d490b0ca690ddd9d62ac4f
gareths <gareths@taggart.pharm.nottingham.ac.uk>
2015-11-17 17:46:52 +0000

    --version, -V, --help, -h

--- setup.py
+++ setup.py
@@ -56,7 +56,7 @@ if not os.path.isdir(os.path.expanduser("~/.Longbow")):
 # setup args
 setup_args = {
     'name': "Longbow",
-    'version': "1.01.000",
+    'version': "1.01.001",
     'description': "Biomolecular simulation remote job submission "
                    "utility.",
     'long_description': "Longbow sends jobs submitted to your desktop to a "
