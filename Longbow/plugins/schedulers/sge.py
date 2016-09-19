4ae37e0738f1dfeb736bb37ecccce2ac03bee3e6
jimboid <james.gebbie@stfc.ac.uk>
2016-09-19 14:45:56 +0100

    fix for using 'is' when comparing strings

--- Longbow/plugins/schedulers/sge.py
+++ Longbow/plugins/schedulers/sge.py
@@ -113,7 +113,7 @@ def prepare(job):
         jobfile.write("#$ -t 1-" + job["replicates"] + "\n")
 
     # Assume MPI for now - OMP could be added later on request.
-    if int(job["cores"]) > 1 or job["sge-peoverride"].lower() is "true":
+    if int(job["cores"]) > 1 or job["sge-peoverride"].lower() == "true":
 
         jobfile.write("#$ -pe " + job["sge-peflag"] + " " +
                       job["cores"] + "\n\n")
@@ -125,7 +125,7 @@ def prepare(job):
             module = module.replace(" ", "")
             jobfile.write("module load {0}\n\n" .format(module))
 
-    if int(job["cores"]) > 1 or job["sge-peoverride"].lower() is "true":
+    if int(job["cores"]) > 1 or job["sge-peoverride"].lower() == "true":
 
         mpirun = job["handler"] + " -n " + job["cores"]
 
