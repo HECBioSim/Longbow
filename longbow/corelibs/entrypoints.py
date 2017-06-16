f0721cbdb59e670a121efc010f59f5566417e936
jimboid <james.gebbie@stfc.ac.uk>
2017-06-16 16:15:29 +0100

    Issue #30
    *Added new BSD license file.
    *Added BSD license to each file.
    *AUTHORS file created.
    *Updated the text outputed on launch
    *Fixed a bug with bash autocomplete file.

--- longbow/corelibs/entrypoints.py
+++ longbow/corelibs/entrypoints.py
@@ -1,22 +1,34 @@
-# Longbow is Copyright (C) of James T Gebbie-Rayet and Gareth B Shannon 2015.
+# BSD 3-Clause License
 #
-# This file is part of the Longbow software which was developed as part of the
-# HECBioSim project (http://www.hecbiosim.ac.uk/).
+# Copyright (c) 2017, Science and Technology Facilities Council and
+# The University of Nottingham
+# All rights reserved.
 #
-# HECBioSim facilitates and supports high-end computing within the UK
-# biomolecular simulation community on resources such as ARCHER.
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
 #
-# Longbow is free software: you can redistribute it and/or modify it under the
-# terms of the GNU General Public License as published by the Free Software
-# Foundation, either version 2 of the License, or (at your option) any later
-# version.
+# * Redistributions of source code must retain the above copyright notice, this
+#   list of conditions and the following disclaimer.
 #
-# Longbow is distributed in the hope that it will be useful, but WITHOUT ANY
-# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
-# A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+# * Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
 #
-# You should have received a copy of the GNU General Public License along with
-# Longbow.  If not, see <http://www.gnu.org/licenses/>.
+# * Neither the name of the copyright holder nor the names of its
+#   contributors may be used to endorse or promote products derived from
+#   this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
 
 """This module contains the Longbow entry points and supporting methods.
 
@@ -174,8 +186,10 @@ def launcher():
                  "HECBioSim project (http://www.hecbiosim.ac.uk/)")
         LOG.info("HECBioSim facilitates high-end biomolecular simulation "
                  "on resources such as ARCHER")
-        LOG.info("Longbow is Copyright (C) of James T Gebbie-Rayet and "
-                 "Gareth B Shannon 2015.")
+        LOG.info("Longbow is Copyright (C) of Science and Technology "
+                 "Facilities Council and The University of Nottingham.")
+        LOG.info("Longbow was created by Dr James T. Gebbie-Rayet, Dr Gareth "
+                 "B. Shannon and Prof Charles A. Laughton.")
         LOG.info("Please cite our paper: Gebbie-Rayet, J, Shannon, G, "
                  "Loeffler, H H and Laughton, C A 2016 Longbow: A "
                  "Lightweight Remote Job Submission Tool. Journal of "
